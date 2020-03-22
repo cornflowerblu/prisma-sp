24b3c752d53cfbf42ae50053262812d98d5a9ccb
Roger <roger.urich@shiftpixy.com>
2020-03-21 21:37:56 -0700

    Add "projects" folder structure

--- projects/blog/index.ts
+++ projects/blog/index.ts
@@ -1,4 +1,4 @@
-import { prisma } from './generated/prisma-client'
+import { prisma } from '../../generated/prisma-client'
 
 // A `main` function so that we can use async/await
 async function main() {
